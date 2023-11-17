0d58512b462c788d12896b83f9a117db52a861da
Pooya Parsa <pooya@pi0.io>
2023-11-17 11:51:20 +0100

    update to nightly

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,46 +1,6 @@
 import { defineNuxtConfig } from "nuxt/config";
 import { version as nuxtVersion } from 'nuxt/package.json'
-import { AWSAmplifyCustomConfig } from "./amplify/types";
-
-declare module 'nitropack' {
-  interface NitroConfig {
-    awsAmplify?: AWSAmplifyCustomConfig
-  }
-}
 
 export default defineNuxtConfig({
-  app: {
-    // baseURL: '/base'
-  },
-  nitro: {
-    // preset: provider === 'aws_amplify' ? "./amplify" : undefined,
-    preset:  "./amplify",
-    awsAmplify: {
-      imageOptimization: {
-        path: "/_nuxt/image",
-        cacheControl: "public, max-age=3600, immutable"
-      },
-      imageSettings: {
-          sizes: [100, 200,300,500,640],
-          domains: [],
-          remotePatterns: [],
-          formats: ["image/jpeg", "image/png", "image/webp", "image/avif"],
-          minimumCacheTTL: 60,
-          dangerouslyAllowSVG: false
-      },
-    },
-    framework: {
-      name: 'nuxt',
-      version: nuxtVersion
-    }
-  },
-  image: {
-    provider: 'amplify',
-    providers: {
-      amplify: {
-        provider: '~/amplify/image-provider.ts',
-      }
-    }
-  },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/image"]
 });

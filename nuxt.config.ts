283d96ff0a7ad7fd69eab1e00f19c7c7b5dd1a7f
Pooya Parsa <pooya@pi0.io>
2023-11-20 11:28:54 +0100

    try full static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 import { defineNuxtConfig } from "nuxt/config";
-import { version as nuxtVersion } from 'nuxt/package.json'
 
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/image"]
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image"],
+  nitro: {
+    preset: './preset'
+  }
 });
